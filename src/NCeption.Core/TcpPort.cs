24a655cf378af36b1755609a107367b8232c09cc
Matt Davis <matt.bronumski@btinternet.com>
2013-04-21 19:13:54 +0100

    Added first test

---
+++ src/NCeption.Core/TcpPort.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace NCeption
+{
+    public static class TcpPort
+    {
+        public static int GetNextFreePort()
+        {
+            var l = new TcpListener(IPAddress.Loopback, 0);
+            try
+            {
+                l.Start();
+
+                return ((IPEndPoint)l.LocalEndpoint).Port;
+            }
+            finally
+            {
+                l.Stop();
+            }
+        }
+    }
+}
---
+++ src/NCeption.Tests/TcpPortFixtures/GetNextFreePort/When_getting_next_free_port.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace NCeption.TcpPortFixtures.GetNextFreePort
+{
+    [TestFixture]
+    class When_getting_next_free_port
+    {
+        private int result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            result = TcpPort.GetNextFreePort();
+        }
+
+        [Test]
+        public void Should_get_a_port_number()
+        {
+            result.Should().BeGreaterThan(0);
+        }
+
+        [Test]
+        public void Should_be_able_to_open_a_socket_with_the_port_number()
+        {
+            var l = new TcpListener(IPAddress.Loopback, result);
+
+            Action act = l.Start;
+            
+            act.ShouldNotThrow<Exception>();
+            
+            l.Stop();
+        }
+    }
+}
