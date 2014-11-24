cff11dc0452a43e4ff71535dad3ea14fde18c8ae
Matt Davis <matt.bronumski@btinternet.com>
2014-11-24 12:19:04 -0500

    Fixing IisExpress project nuget packaging

--- src/NCeption.IisExpress/Properties/AssemblyInfo.cs
+++ src/NCeption.IisExpress/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NCeption.IisExpress")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("NCeption components for spinning up an instance of an Asp.Net application in IIS Express")]
 [assembly: ComVisible(false)]
 [assembly: Guid("34087b7a-c02f-4f0e-afa7-c318b9ccf152")]
