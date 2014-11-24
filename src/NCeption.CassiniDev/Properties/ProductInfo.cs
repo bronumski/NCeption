84308dedb3f4bc8a865ba66fb7788867cb17184c
Matt Davis <matt.bronumski@btinternet.com>
2014-11-24 11:41:06 -0500

    Fixed assembly attributes

--- src/NCeption.CassiniDev/Properties/ProductInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: CLSCompliant(true) ]
-[assembly: AssemblyProduct("NCeption")]
-[assembly: AssemblyCompany("NCeption")]
-[assembly: AssemblyCopyright("Copyright © NCeption 2014")]
-[assembly: AssemblyTrademark("NCeption")]
-[assembly: AssemblyCulture("") ]
--- src/NCeption.Core/Properties/ProductInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Reflection;
-
-[ assembly: CLSCompliant( true ) ]
-[assembly: AssemblyProduct("NCeption")]
-[assembly: AssemblyCompany("NCeption")]
-[assembly: AssemblyCopyright("Copyright © NCeption 2013")]
-[ assembly: AssemblyTrademark( "" ) ]
-[ assembly: AssemblyCulture( "" ) ]
