f50ee2dff5305ecbce026d8b8b381a640b6a959c
Matt Davis <matt.bronumski@btinternet.com>
2014-11-24 11:30:21 -0500

    IISExpress and Cassini build nuget packages. Added Owin wrapper for hosting owin services.

---
+++ src/NCeption.Owin/OwinStartableService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace NCeption.Owin
+{
+    public class OwinStartableService<TStartUp> : IStartableService
+    {
+        private IDisposable owinTestServer;
+
+        public void Start()
+        {
+            owinTestServer = WebApp.Start<TStartUp>(UriManager.GetUriForKey(GetType().FullName).ToString());
+        }
+
+        public void Stop()
+        {
+            Safely.Dispose(owinTestServer);
+        }
+    }
+}
---
+++ src/NCeption.Tests/Web/OwinHosting/When_starting_an_aspnet_web_project.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Contexts;
+using FluentAssertions;
+using NCeption.NUnit;
+using NCeption.Owin;
+using NUnit.Framework;
+using Owin;
+
+namespace NCeption.Web.OwinHosting
+{
+    [RequireServices(typeof(OwinTestProject))]
+    class When_starting_an_owin_web_project : IRequireHttpClient<When_starting_an_owin_web_project.OwinTestProject>
+    {
+        [Test]
+        public void Should_deploy_website_and_run_it()
+        {
+            HttpClient.GetAsync("/Home/Index").Result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        public HttpClient HttpClient { set; private get; }
+
+        class OwinTestProject : OwinStartableService<OwinTestStartup>
+        {
+        }
+
+        class OwinTestStartup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                app.Run(context => context.Response.WriteAsync("Hello, world"));
+            }
+        }
+    }
+}
